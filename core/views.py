d6f6afd62a7205cc6c306654922efbcdbbe8867f
Ibrokhimc <ibrokhmali@gmail.com>
2022-11-26 18:52:47 +0500

    Single Portfolio pages upgraded.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from news.models import Guruxlash, Post, Portfolio
-from django.core.mail import send_mail
 
 # Create your views here.
 
